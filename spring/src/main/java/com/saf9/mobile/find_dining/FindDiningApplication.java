4e56c51da772352df16ea5a818b57c4c49de47a7
lee <lee@gmail.com>
2023-05-07 22:28:40 +0900

    ft : spring & mysql docker connection

---
+++ spring/src/main/java/com/saf9/mobile/find_dining/FindDiningApplication.java
@@ -0,0 +1,13 @@
+package com.saf9.mobile.find_dining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindDiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindDiningApplication.class, args);
+	}
+
+}
---
+++ spring/src/test/java/com/saf9/mobile/find_dining/FindDiningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saf9.mobile.find_dining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindDiningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
